4b8c33c0f30be6499a46e2057b7757ed988bdffb
alaa <alaa2amz@gmail.com>
2017-12-27 21:55:59 +0200

    17.12.27.4 09:55pm after solving annular

--- infonity.Start.17.12.25.2/HomeViewController.swift
+++ infonity.Start.17.12.25.2/HomeViewController.swift
@@ -12,20 +12,33 @@ class HomeViewController: UIViewController {
     func getFivCircularLowerFButtonsAroundCenterWithSideMargin(sideMargin : CGFloat) -> [UIButton]
     {
         var buttons = [UIButton]()
-        for No in 1..<5
+        for No in 0..<5
         {
+           
             let oneButton = UIButton()
+            self.view.addSubview(oneButton)
             oneButton.backgroundColor = UIColor.red
             oneButton.frame.size = CGSize(width: 10, height: 10)
-            let myX :CGFloat = self.view.frame.width - sideMargin + (self.view.frame.width - 2 * sideMargin / 4) *   CGFloat( No)
+           // let myX :CGFloat =  sideMargin + ((self.view.frame.width - 2 * sideMargin) / 4) *   CGFloat( No)
             let r :CGFloat = (self.view.frame.width - 2 * sideMargin)/2
+            
+          
+            let x1 :CGFloat = 0.0
+            let x2:CGFloat =  r * 1 / sqrt(2) / 2
+            let x3:CGFloat =  r
+            let x4:CGFloat = r + r - (r * 1 / sqrt(2) / 2 )
+            let x5:CGFloat = 2 * r
+            let xx :[CGFloat] = [x1,x2,x3,x4,x5]
+            
+            let myX :CGFloat =  sideMargin + CGFloat( xx[No])
+            print(String(describing: myX))
             let h :CGFloat = self.view.center.x
             let k :CGFloat = self.view.center.y
-            let sq_R_sq_Xh : CGFloat = pow(r, 2) - pow((myX-h), 2)
+            let sq_R_sq_Xh : CGFloat = pow(r, 2) - pow((myX - h), 2)
             let sqrt_sq_R_sq_Xh : CGFloat = sqrt(sq_R_sq_Xh)
             let myY : CGFloat = sqrt_sq_R_sq_Xh + k
             oneButton.center = CGPoint(x: myX, y: myY)
-            self.view.addSubview(oneButton)
+            
             buttons.append(oneButton)
             
             
@@ -35,7 +48,7 @@ class HomeViewController: UIViewController {
     }
 
     override func viewDidLoad() {
-        let menuButtons = getFivCircularLowerFButtonsAroundCenterWithSideMargin(sideMargin: 10)
+        let menuButtons = getFivCircularLowerFButtonsAroundCenterWithSideMargin(sideMargin: 20)
         
         super.viewDidLoad()
 

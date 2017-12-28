a200b37ed56cd7fa067c0fb0742b152a00bf1684
alaa <alaa2amz@gmail.com>
2017-12-28 18:15:37 +0200

    17.12.28.5 06:15pm last of day

---
+++ CellView.swift
@@ -0,0 +1,15 @@
+//
+//  CellView.swift
+//  infonity.Start.17.12.25.2
+//
+//  Created by A on 12/28/17.
+//  Copyright © 2017 Arena. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class CellView: JTAppleDayCellView {
+    @IBOutlet var dayLabel: UILabel!
+}
